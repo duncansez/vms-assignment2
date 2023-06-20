cbb27e10c47ba761215e3c44a1e1cd6a4bc46a61
duncansez <soberybasri@gmail.com>
2023-06-20 22:43:47 +0800

    remove weatherforecast sample and add CRUD method in vehicle api controller

--- back-end/VMS/VMS/Controllers/VehicleController.cs
+++ back-end/VMS/VMS/Controllers/VehicleController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VMS.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,7 +14,94 @@ namespace VMS.Controllers
     [Route("api/[controller]")]
     public class VehicleController : ControllerBase
     {
-        
+        private readonly VMSDbContext _context;
+
+        public VehicleController(VMSDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/vehicle
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vehicle>>> GetVehicles()
+        {
+            return await _context.Vehicle.ToListAsync();
+        }
+
+        // GET: api/vehicle/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vehicle>> GetVehicle(int id)
+        {
+            var vehicle = await _context.Vehicle.FindAsync(id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return vehicle;
+        }
+
+        // POST: api/vehicle
+        [HttpPost]
+        public async Task<ActionResult<Vehicle>> CreateVehicle(Vehicle vehicle)
+        {
+            _context.Vehicle.Add(vehicle);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVehicle", new { id = vehicle.Id }, vehicle);
+        }
+
+        // PUT: api/vehicle/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, Vehicle vehicle)
+        {
+            if (id != vehicle.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vehicle).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehicleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/vehicle/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await _context.Vehicle.FindAsync(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vehicle.Remove(vehicle);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VehicleExists(int id)
+        {
+            return _context.Vehicle.Any(v => v.Id == id);
+        }
     }
 }
 
